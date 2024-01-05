1132f31da9c99ab6bf5ee4ac6f9feb22366a4289
ajithvallabai <inocajith21.5@gmail.com>
2024-01-05 22:38:02 +0530

    Update Readme

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # Interstate_Traffic_Volume_Project
-Metro Interstate traffic volume project (MLOps app)
+Metro Interstate traffic volume project
+
+### Tools Used
+
+<p float="left">
+    <img src="./assets/cassandra.png" width="100" height="100"/>
+    <img src="./assets/tensorflowkeras.jpg" width="200" height="100"/>
+    <img src="./assets/dvc.png" width="100" height="100"/>
+    <img src="./assets/flask.png" width="100" height="100"/>
+</p>
+<p float="left">
+    <img src="./assets/docker.png" width="100" height="100"/>
+    <img src="./assets/cicd.jpg" width="200" height="100"/>
+    <img src="./assets/AWS_EC2.png" width="100" height="100"/>
+    <img src="./assets/AWS_ECR.png" width="100" height="100"/>
+</p>
 
 
 ## Workflows
 
-- Update config.yaml
-- Update secrets.yaml [Optional]
-- Update params.yaml
-- Update the entity
-- Update the configuration manager in src config
-- Update the components
-- Update the pipeline
-- Update the main.py
-- Update the dvc.yaml
+- config.yaml
+- secrets.yaml [Optional]
+- params.yaml
+- Entity
+- Configuration manager in src config
+- Components
+- Pipeline
+- main.py
+- dvc.yaml
 
 ### Installation
 
@@ -25,6 +40,10 @@ Metro Interstate traffic volume project (MLOps app)
 ### To Run
 - python app.py
 
-ECR  REPO
+### Update configuration
+- Update params.yaml with configuration which is apt for your training eg: batch size to 500 and epochs to 200.
 
-306093656765.dkr.ecr.us-east-1.amazonaws.com/trafficvolume
+### Documents
+
+- [Link here](./documents)
+
--- main.py
+++ main.py
@@ -5,28 +5,28 @@ from trafficVolumePrediction.pipeline.stage_03_training import ModelTrainingPipe
 from trafficVolumePrediction.pipeline.stage_04_evaluation import EvaluationPipeline
 from trafficVolumePrediction.pipeline.predict import PredictionPipeline
 
-# STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion stage"
 
-# try:
-#     logger.info(">>>>>>> stage {STAGE_NAME} started <<<<<")
-#     obj = DataIngestionTrainingPipeline()
-#     obj.main()
-#     logger.info(">>>>>>> stage {STAGE_NAME} completed <<<<< \n =========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(">>>>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(">>>>>>> stage {STAGE_NAME} completed <<<<< \n =========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = "Prepare base model"
+STAGE_NAME = "Prepare base model"
 
-# try:
-#     logger.info(f"*************************")
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     obj = PrepareBaseModelTrainingPipeline()
-#     obj.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\nx========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f"*************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 STAGE_NAME = "Training"
 
@@ -40,18 +40,18 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-# STAGE_NAME = "Evaluation stage"
+STAGE_NAME = "Evaluation stage"
 
-# try:
-#     logger.info(f"*************************")
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     model_evaluation = EvaluationPipeline()
-#     model_evaluation.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\nx========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f"*************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evaluation = EvaluationPipeline()
+    model_evaluation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 import numpy as np
-model = PredictionPipeline(np.array([[0,288.28,0.0,0.0,40,0,0,2012,10,2,9]]))
-print(model.predict())
+model = PredictionPipeline()
+print(model.predict(np.array([[0,288.28,0.0,0.0,40,0,0,2012,10,2,9]])))
